53b02eb6355af84384f96456375a818cc7e9e5c1
saul basilio <saulbf96@outlook.es>
2025-10-01 00:54:05 -0600

    SwitchStatement

---
+++ ExerciseIfSwitchStatement/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ Pedir al usuario su edad  
+eres menor de edad si es <18 
+eres adulto  si esta entre 18 y 65
+eres adulto mayor si  es mayor de 65 
+ */
+
+//declaramos variable 
+int edad;
+
+// pedimos al usuario que ingrese su edad.
+Console.WriteLine("Ingresa tu edad:");
+
+edad = Convert.ToInt32(Console.ReadLine());// aqui asignamos lo que ingreso el usuario a edad.
+
+//aqui evaluamos la edad y mostramos el mensaje como nos indica usare if 
+
+if (edad < 18)
+{
+    //si edad es menor a 18 
+    Console.WriteLine("Eres menor de edad!");
+}
+else if (edad >= 18 && edad <= 65)
+{
+    //si edad es mayor a 18 pero es igual o menor a 65 
+    //aqui se usa && = AND  es verdadero si los dos cumplen con la evaluacion 
+    // y || = O esto es verdadero con solo una de las dos si cumpla la evaluacion
+    Console.WriteLine("eres adulto");
+}
+else if (edad > 65)
+{
+    Console.WriteLine("Eres adulto mayor");
+}
--- SwithStatement/Program.cs
+++ SwithStatement/Program.cs
@@ -5,4 +5,30 @@
 //La sentencia break aquí simplemente termina la sentencia switch, y el procesamiento continúa en la sentencia 
 //que sigue a la estructura
 
+//ejemplo sencillo se switch 
 
+//declaramos constantes;
+using System.Diagnostics;
+
+const string myName = "saul";
+const string niceName = "yazmin";
+const string sillName = "pechuga";
+string name;
+//pedimos ingresar el nombre 
+Console.WriteLine("whats is your name?");
+name = Console.ReadLine();
+
+switch (name.ToLower())//ToLower lo que hace es covertit texto en minusculas 
+{
+	case myName:
+		Console.WriteLine("You have the same name as me ");
+		break; //recordar cuando se cumple llega aqui y ya no sigue las demas instrucciones.
+	case niceName:
+		Console.WriteLine("Good, What a nice name you have!");
+		break;
+	case sillName:
+		Console.WriteLine("That's  a very silly name");
+		break;
+}
+Console.WriteLine($"Hello {name}");
+Console.ReadKey();
